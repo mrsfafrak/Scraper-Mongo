6ffa036a7881432124809f61d0ba563dd447c233
Tori <victoria.fafrak@gmail.com>
2019-05-31 20:06:22 -0400

    update

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,38 +1,14 @@
 var request = require("request");
 var cheerio = require("cheerio");
 
-// var scrape = function (cb) {
-//     request("http://www.nytimes.com", function(err, res, body){
-//         var $ = cheerio.load(body);
-//         var articles = [];
-
-//         $(".theme-summary").each(function(i, element){
-//             var head = $(this).children(".story-heading").text().trim();
-//             var sum = $(this).children(".summary").text().trim();
-
-//             if(head && sum){
-//                 var headNeat = head.replace(/(\r\n|\n|\r|\t|\s)+/gm, " ").trim();
-//                 var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s)+/gm, " ").trim();
-
-//                 var dataToAdd = {
-//                     headline: headNeat,
-//                     summary: sumNeat
-//                 };
-
-//                 articles.push(dataToAdd);
-//             }
-//         });
-//         cb(articles);
-//     });
-// };
 var scrape = function (cb) {
-    request("https://www.npr.org/sections/news/", function(err, res, body){
+    request("http://www.nytimes.com", function(err, res, body){
         var $ = cheerio.load(body);
         var articles = [];
 
-        $(".info-item").each(function(i, element){
-            var head = $(this).children(".title").text().trim();
-            var sum = $(this).children(".teaser").text().trim();
+        $(".theme-summary").each(function(i, element){
+            var head = $(this).children(".story-heading").text().trim();
+            var sum = $(this).children(".summary").text().trim();
 
             if(head && sum){
                 var headNeat = head.replace(/(\r\n|\n|\r|\t|\s)+/gm, " ").trim();
